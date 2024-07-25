4b3fb7139c83b352ec536ab9bc801a7d15312df1
busefilizler <b.filizler@digitastic.app>
2024-07-25 14:26:03 +0300

    router view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <v-app class="!bg-[#212121] text-white h-screen">
     <v-main>
-      <MoviePage />
+      <RouterView />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  import MoviePage from './pages/MoviePage.vue'
 </script>
